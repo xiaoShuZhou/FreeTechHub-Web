872e7c4c7fada26c3cc613ab857d848c687de668
YKK-cp <1347256906@qq.com>
2020-08-15 14:23:05 +0800

    add delete comment function of blog

--- src/components/ShowComments.vue
+++ src/components/ShowComments.vue
@@ -8,6 +8,9 @@
 				<div class="card">
 					<div v-if="is_root == false">
 						<h2 class="card-title">By {{ comment_tree.comment.owner }} at {{ comment_tree.comment.time }}</h2>
+            <div v-if="comment_tree.comment.owner == user">
+              <button @click="Delete(comment_tree.comment)">Delete</button>
+            </div>
 						<p class="content" v-html='comment_tree.comment.content'></p>
 						<div v-if="comment_tree.comment.status == false">
 							<button @click="ReplyTo(comment_tree.comment)">Reply To</button>
@@ -31,6 +34,7 @@
 
 <script>
 import Comment from '@/assets/utils/models/Comment'
+import {login_required} from '@/assets/utils/auth'
 
 export default {
 	name:"ShowComments",
@@ -40,63 +44,80 @@ export default {
 			comment_tree:'',
 			comment_content:'',
 			sub_comment_content:'',
-			owner:''
+      owner:'',
+      user:'',
 		}
 	},
 	methods: {
-			_getComment(comment){
-				return new Comment({
-					id:this.id,
-					content:this.comment_content,
-					owner: this.owner,
-					sub_comments_of:comment.pk
-				})
-			},
+    _getComment(comment){
+      return new Comment({
+        id:this.id,
+        content:this.comment_content,
+        owner: this.owner,
+        sub_comments_of:comment.pk
+      })
+    },
 
-			_getSubComment(comment){
-				return new Comment({
-					id:this.id,
-					content:this.sub_comment_content,
-					owner:this.owner,
-					sub_comments_of:comment.pk
-				})
-			},
+    _getSubComment(comment){
+      return new Comment({
+        id:this.id,
+        content:this.sub_comment_content,
+        owner:this.owner,
+        sub_comments_of:comment.pk
+      })
+    },
 
-			ReplyTo(comment){
-				comment.status = true
-			},
+    ReplyTo(comment){
+      comment.status = true
+    },
 
-			Cancel(comment){
-				comment.status = false
-			},
+    Cancel(comment){
+      comment.status = false
+    },
 
-			saveComment(comment){
-				let parent_comment = this._getComment(comment)
-				parent_comment.save().then(() => {
-					Comment.query_sub_comments(this.root_id)
-					.then(comment_tree => {
-						let wrapped_comment_tree = Comment.wrap_sub_comments(comment_tree)
-						this.$store.commit('set_root_comment_tree', wrapped_comment_tree)
-						this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.root_id)
-					})
-				})
-			},
+    setCommentTree(comment_tree){
+      let wrapped_comment_tree = Comment.wrap_sub_comments(comment_tree)
+      this.$store.commit('set_root_comment_tree', wrapped_comment_tree)
+      this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.root_id)
+    },
 
-			saveSubComment(comment){
-				comment.status = false
-				let sub_comment = this._getSubComment(comment)
-				sub_comment.save().then(() => {
-					Comment.query_sub_comments(this.$store.state.root_id)
-					.then(comment_tree => {
-						let wrapped_comment_tree = Comment.wrap_sub_comments(comment_tree)
-						this.$store.commit('set_root_comment_tree', wrapped_comment_tree)
-						this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.root_id)
-					})
-				})
-			}
+    saveComment(comment){
+      let parent_comment = this._getComment(comment)
+      parent_comment.save().then(() => {
+        Comment.query_sub_comments(this.root_id)
+        .then(comment_tree => {
+          this.setCommentTree(comment_tree)
+        })
+      })
+    },
+
+    saveSubComment(comment){
+      comment.status = false
+      let sub_comment = this._getSubComment(comment)
+      sub_comment.save().then(() => {
+        Comment.query_sub_comments(this.$store.state.root_id)
+        .then(comment_tree => {
+          this.setCommentTree(comment_tree)
+        })
+      })
+    },
+    
+    Delete(comment){
+      comment.delete().then(() => {
+        Comment.query_sub_comments(this.$store.state.root_id)
+        .then(comment_tree => {
+          let wrapped_comment_tree = Comment.wrap_sub_comments(comment_tree)
+          this.$store.commit('set_root_comment_tree', wrapped_comment_tree)
+          this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.$store.state.root_id)
+        })
+      })
+    }
 	},
 	created() {
-		this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.root_id)
+    login_required(this, user => {
+      this.user = user.pk
+    })
+    this.comment_tree = Comment.get_matched_comment_tree(this.$store.state.root_comment_tree, this.root_id)
     }
 }
 </script>
